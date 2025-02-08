6d5da3d84a0f3413201be7418656e629f826a249
ちとせあめ <chitose.dev@outlook.jp>
2025-02-08 16:17:24 +0900

    エラーハンドリングの追加

--- routes/freednsRoutes.js
+++ routes/freednsRoutes.js
@@ -3,7 +3,11 @@ const router=express.Router();
 const FreeDNS=require("../freeDNS");
 
 router.get("/",async(req,res)=>{
-    res.render("domains",{domains:(await FreeDNS.getRegistry(req.query?.page)).map(item=>`<a target="_blank" rel="noopener noreferrer" href="${item}">${item}<br></a>`).join("")});
+    try{
+        res.render("domains",{domains:(await FreeDNS.getRegistry(req.query?.page)).map(item=>`<a target="_blank" rel="noopener noreferrer" href="${item}">${item}<br></a>`).join("")});
+    }catch(e){
+        res.send(e);
+    }
 })
 
 module.exports=router;
